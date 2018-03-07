8d0f614700a2286b2c970e6356d046b9edbf347e
Manoj Shinde <shinde.m@husky.neu.edu>
2018-03-07 03:04:01 -0500

    Added Spring Boot Project

---
+++ bookmyshow/src/main/java/com/bookmyshow/BookmyshowApplication.java
@@ -0,0 +1,12 @@
+package com.bookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyshowApplication.class, args);
+	}
+}
---
+++ bookmyshow/src/test/java/com/bookmyshow/BookmyshowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookmyshow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookmyshowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
